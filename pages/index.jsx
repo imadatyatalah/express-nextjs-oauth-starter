3477126baa9f0344939be418fc750b1e416c3734
imad <imadatyatalah@gmail.com>
2021-06-11 11:24:45 +0100

    init

---
+++ pages/index.jsx
@@ -0,0 +1,55 @@
+import { parseCookies } from "nookies";
+
+const Home = ({ data }) => {
+  return (
+    <>
+      <main>
+        <h1>Welcome to Nextjs OAuth with GitHub</h1>
+
+        <div>
+          {data.token ? (
+            <p>
+              <a href={"http://localhost:1337/auth/github/logout"}>
+                Click here to logout
+              </a>
+            </p>
+          ) : (
+            <p>
+              <a href={"http://localhost:1337/auth/github"}>
+                Click here to login
+              </a>
+            </p>
+          )}
+        </div>
+
+        <code>
+          <pre>{JSON.stringify(data.user, null, 2)}</pre>
+        </code>
+      </main>
+    </>
+  );
+};
+
+async function getUser(token) {
+  const res = await fetch("http://localhost:1337/me", {
+    headers: { token },
+  });
+
+  const data = await res.json();
+
+  if (res.status === 200) {
+    return { token, user: data };
+  } else {
+    return { token };
+  }
+}
+
+export const getServerSideProps = async (ctx) => {
+  const { jwtToken } = parseCookies(ctx);
+
+  const data = await getUser(jwtToken || "");
+
+  return { props: { data } };
+};
+
+export default Home;
