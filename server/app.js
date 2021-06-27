34782e30b496f386bdcf17eea0d833e812596613
imad <imadatyatalah@gmail.com>
2021-06-27 10:07:51 +0100

    fix(server): Enable cors again Don't commit your changes before you test them

--- server/app.js
+++ server/app.js
@@ -17,6 +17,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
