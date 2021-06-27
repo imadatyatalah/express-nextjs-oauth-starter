3f02a64e2ae2821f0cd3e10418c6dfc5f7ded746
imad <imadatyatalah@gmail.com>
2021-06-27 10:10:32 +0100

    feat(client): Run zustand's signIn func in each page

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+
+import { useUser } from "../hooks/useUser";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
+  const signIn = useUser((state) => state.signIn);
+
+  useEffect(() => {
+    signIn();
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
