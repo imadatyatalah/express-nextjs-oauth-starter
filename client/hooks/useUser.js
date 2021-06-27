83b83e29145d9e66699b716eb4ae8c4b2a76ce09
imad <imadatyatalah@gmail.com>
2021-06-27 10:09:55 +0100

    feat(client): Store the user in zustand instead of useState

--- client/hooks/useUser.js
+++ client/hooks/useUser.js
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
-
 import { parseCookies } from "nookies";
+import create from "zustand";
 
-const getUser = async () => {
-  const { gh_token } = parseCookies();
+export const useUser = create((set) => ({
+  user: null,
+  signIn: async () => {
+    const { gh_token } = parseCookies();
 
-  const res = await fetch("http://localhost:1337/me", {
-    headers: { token: gh_token },
-  });
+    const res = await fetch("http://localhost:1337/me", {
+      headers: { token: gh_token },
+    });
 
-  const data = await res.json();
-
-  return data;
-};
-
-const useUser = () => {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    getUser().then((res) => setUser(res));
-  }, []);
-
-  return { user };
-};
-
-export default useUser;
+    set({ user: await res.json() });
+  },
+}));
