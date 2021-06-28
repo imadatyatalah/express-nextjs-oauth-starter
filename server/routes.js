d17b8c4ca2f18604949c01f651abb0527bcb875a
imad <imadatyatalah@gmail.com>
2021-06-28 13:54:24 +0100

    Remove users route

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const routes = express.Router();
 
-const usersRouter = require("./routes/users");
 const currentUserRouter = require("./routes/me");
 const githubOAuth = require("./routes/auth/github");
 const user = require("./routes/user");
 
-routes.use("/users", usersRouter);
 routes.use("/me", currentUserRouter);
 routes.use("/auth/github", githubOAuth);
 routes.use("/user", user);
