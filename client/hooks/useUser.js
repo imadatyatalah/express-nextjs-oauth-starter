94bf79060a351791ffcf8d63ee4b985e8bf42b2d
imad <imadatyatalah@gmail.com>
2021-06-28 13:18:13 +0100

    feat(client): Added logged_in bool

--- client/hooks/useUser.js
+++ client/hooks/useUser.js
@@ -3,13 +3,19 @@ import create from "zustand";
 
 export const useUser = create((set) => ({
   user: null,
+  logged_in: false,
   signIn: async () => {
     const { gh_token } = parseCookies();
 
     const res = await fetch("http://localhost:1337/me", {
       headers: { token: gh_token },
     });
+    const user = await res.json();
 
-    set({ user: await res.json() });
+    if (!user.error) {
+      set({ logged_in: true });
+    }
+
+    set({ user });
   },
 }));
