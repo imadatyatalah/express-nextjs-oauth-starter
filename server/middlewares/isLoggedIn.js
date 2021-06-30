b55f3ec700c77bd63163a7b4b1c848e57aaa3583
imad <imadatyatalah@gmail.com>
2021-06-30 19:44:05 +0100

    fix(server): Use jwt_key env var

--- server/middlewares/isLoggedIn.js
+++ server/middlewares/isLoggedIn.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
-const JWT_KEY = "something_private_and_long_enough_to_secure";
+import { jwt_key } from "../config/credentials.js";
 
 const isLoggedIn = (req, res, next) => {
   const token = req.headers["token"];
 
-  jwt.verify(token, JWT_KEY, (err, data) => {
+  jwt.verify(token, jwt_key, (err, data) => {
     if (err) {
       res.status(401).send({ error: true, message: "Unauthorized" });
     } else {
