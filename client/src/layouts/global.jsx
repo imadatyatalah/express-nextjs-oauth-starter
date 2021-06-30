00bf66a46953b45c9cfb9f563dec9ba8c3e1c4b5
imad <imadatyatalah@gmail.com>
2021-06-30 15:19:33 +0100

    fix(client): Fix some ESLint warnings

--- client/src/layouts/global.jsx
+++ client/src/layouts/global.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+import Header from "@/components/header";
 
 const Global = ({ children }) => {
   return (
     <>
-      <header></header>
+      <Header />
       <main>{children}</main>
       <footer></footer>
     </>
   );
+};
+
+Global.propTypes = {
+  children: PropTypes.node,
 };
 
 export default Global;
--- client/src/pages/u/[username]/index.jsx
+++ client/src/pages/u/[username]/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Username = ({ user }) => {
   return (
@@ -22,6 +23,10 @@ export const getServerSideProps = async (ctx) => {
   const user = await fetch(`http://localhost:1337/user/${username}`);
 
   return { props: { user: await user.json() } };
+};
+
+Username.propTypes = {
+  user: PropTypes.object.isRequired,
 };
 
 export default Username;
