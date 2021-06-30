286c90d5c56ad05689314dea5b5e7364c1dc03f3
imad <imadatyatalah@gmail.com>
2021-06-30 13:10:35 +0100

    fix(client): Clean up

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,38 +1,7 @@
-import { useUser } from "../stores/useUser";
-
 const Home = () => {
-  const user = useUser((state) => state.user);
-  const logged_in = useUser((state) => state.logged_in);
-
   return (
     <>
-      <section>
-        <h1>Welcome to Nextjs OAuth with GitHub</h1>
-
-        <div>
-          {logged_in && user && (
-            <code>
-              <pre>{JSON.stringify(user, null, 2)}</pre>
-            </code>
-          )}
-        </div>
-
-        <div>
-          {logged_in ? (
-            <p>
-              <a href={"http://localhost:1337/auth/github/logout"}>
-                Click here to logout
-              </a>
-            </p>
-          ) : (
-            <p>
-              <a href={"http://localhost:1337/auth/github"}>
-                Click here to login
-              </a>
-            </p>
-          )}
-        </div>
-      </section>
+      <section></section>
     </>
   );
 };
