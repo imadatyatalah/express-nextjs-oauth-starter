4537e6bb8f6c7666c2bbd377917ec782f9e859c6
imad <imadatyatalah@gmail.com>
2021-06-30 15:18:51 +0100

    feat(client): Added header component

---
+++ client/src/components/header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+
+import { useUser } from "@/stores/useUser";
+
+const Header = () => {
+  const user = useUser((state) => state.user);
+  const logged_in = useUser((state) => state.logged_in);
+
+  return (
+    <>
+      <header className="flex items-center justify-between">
+        <div>
+          <h1 className="font-bold text-4xl">LOGO</h1>
+        </div>
+
+        <div>
+          {logged_in && user ? (
+            <>
+              <div>
+                <Image
+                  src={user.githubAvatarUrl}
+                  width="45"
+                  height="45"
+                  alt={user.githubName}
+                  className="rounded-full"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <button>
+                <a href={"http://localhost:1337/auth/github"}>Sign in</a>
+              </button>
+            </>
+          )}
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
