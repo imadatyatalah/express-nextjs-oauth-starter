0c09a4ca79b4cfd43f12153a5840b2ba34262758
imad <imadatyatalah@gmail.com>
2021-07-01 11:38:55 +0100

    fix(server): Fix typo

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import currentUserRouter from "./routes/me.js";
+import currentUser from "./routes/me.js";
 import githubOAuth from "./routes/auth/github.js";
 import user from "./routes/user/index.js";
 
 const routes = express.Router();
 
-routes.use("/me", currentUserRouter);
+routes.use("/me", currentUser);
 routes.use("/auth/github", githubOAuth);
 routes.use("/user", user);
 
