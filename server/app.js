8173e231a7f47a0100a3e8507c73a9b67efcb6e9
imad <imadatyatalah@gmail.com>
2021-07-02 21:18:17 +0100

    feat(server): Use helmet

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import logger from "morgan";
 import passport from "passport";
 import cors from "cors";
+import helmet from "helmet";
 
 import routes from "./routes.js";
 import "./config/passport.js";
@@ -18,6 +19,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
+app.use(helmet());
 
 app.use(passport.initialize());
 app.use(passport.session());
